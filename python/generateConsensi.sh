7342749af7250e5017f7de622bae76da856fcb0b
mondegreen <54181197+mondegreen@users.noreply.github.com>
2022-07-05 12:14:42 -0400

    Update generateConsensi.sh
    Added new parameters, removed a possible error condition when working with large datasets, added the ability to restart the script from its last position

--- python/generateConsensi.sh
+++ python/generateConsensi.sh
@@ -2,30 +2,50 @@ task(){
   BASE=`basename $1 -subreads.fq`
   DIR=`dirname $1`
 
-  CONSENT-correct --in ${DIR}/${BASE}-subreads.fq \
-    --out ${DIR}/${BASE}-corrected.fa --type ONT
+  if test -f ${DIR}/${BASE}.done; then
+    echo "skipping ${DIR}/${BASE}"
+  else
+    CONSENT-correct --in ${DIR}/${BASE}-subreads.fq \
+      --out ${DIR}/${BASE}-corrected.fa --type ONT
 
-  prank -F -o=${DIR}/${BASE}-corrected \
-    -d=${DIR}/${BASE}-corrected.fa > /dev/null
+    prank -F -o=${DIR}/${BASE}-corrected \
+      -d=${DIR}/${BASE}-corrected.fa > /dev/null
 
-  python smush-msa.py --msa ${DIR}/${BASE}-corrected.best.fas \
-    > ${DIR}/${BASE}-corrected-smushed.best.fas
+    python3 smush-msa.py --msa ${DIR}/${BASE}-corrected.best.fas \
+      > ${DIR}/${BASE}-corrected-smushed.best.fas
 
-  python improve-msa.py --msa ${DIR}/${BASE}-corrected-smushed.best.fas \
-    --max-cores 2 --word-size 12 > ${DIR}/${BASE}-smushed-improved.fa
+    python3 improve-msa.py --msa ${DIR}/${BASE}-corrected-smushed.best.fas \
+      --max-cores 2 --word-size 12 > ${DIR}/${BASE}-smushed-improved.fa
 
-  echo "working on ${DIR}/${BASE}-subreads"
-  python make-consensus.py --msa ${DIR}/${BASE}-smushed-improved.fa \
-    --read ${BASE} > ${DIR}/${BASE}-consensus.fa
+    echo "working on ${DIR}/${BASE}-subreads"
+    python3 make-consensus.py --msa ${DIR}/${BASE}-smushed-improved.fa \
+      --read ${BASE} > ${DIR}/${BASE}-consensus.fa
+
+    touch ${DIR}/${BASE}.done
+  fi
 }
 
-N=25
+# directory to extract subreads from and should include read# directory:
+# for example barcode06/reads5
+DIRR=$1
+# number of processors to use
+N=$2
+# name of output file
+out=$3
 
-for FILE in reads5/*/*-subreads.fq
+printenv PATH
+
+for FILE in $DIRR/*/*-subreads.fq
   do
     ((i=i%N));
     ((i++==0)) && wait
     task $FILE&
   done
-  cat reads5/*/*-consensus.fa > consensi-5plus-subreads.fa
+  cat $DIRR/*/*-consensus.fa > $out
+done
+
+for FILE in $DIRR/*/*-consensus.fa
+  do
+    cat $FILE > $out
+  done
 done
