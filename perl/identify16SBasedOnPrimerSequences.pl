d0e6dc08e639b0b2b3a70e9589b4aa69462b957b
mondegreen <54181197+mondegreen@users.noreply.github.com>
2023-07-27 17:04:59 -0400

    Update identify16SBasedOnPrimerSequences.pl
    Adding pass-through for rocitsPath parameter

--- perl/identify16SBasedOnPrimerSequences.pl
+++ perl/identify16SBasedOnPrimerSequences.pl
@@ -3,17 +3,18 @@ use FileUtil;
 my $rocITSFasta = shift;
 my $primerA = shift;
 my $primerB = shift;
+my $rocitsPath = shift;
 
 ### default primers to identify 16S genes in eubacteria
 $primerA = "TACGG.TACCTTGTTACGACTT" unless defined($primerA);
 $primerB = "CTGAGCCAGGATCAAACTCT" unless defined($primerB);
 
-my $rfh = FileUtil::openFileHandle("perl reformatFasta.pl $rocITSFasta 0 999999999|");
+my $rfh = FileUtil::openFileHandle("perl $rocitsPath/reformatFasta.pl $rocITSFasta 0 999999999|");
 while (<$rfh>) {
   if (/^>/) {
     $def = $_;
   } else {
-    if (/($primerA.*$primerB)/o) {
+    if (/.*($primerA.*$primerB)/o) {
       print $def;
       print $1,"\n";
     }
